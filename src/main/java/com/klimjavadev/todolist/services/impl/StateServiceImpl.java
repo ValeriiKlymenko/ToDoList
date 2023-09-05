1ba8e5d2865cc6b4ef4e4e0ec815e83f84fc1451
KlimJavaDev <klimjavadev@gmail.com>
2023-09-05 12:27:27 +0300

    Додано імплементацію services : RoleService, StateService, TaskService, ToDoService, UserService

---
+++ src/main/java/com/klimjavadev/todolist/services/impl/StateServiceImpl.java
@@ -0,0 +1,61 @@
+package com.klimjavadev.todolist.services.impl;
+
+import com.klimjavadev.todolist.exceptions.NullEntityReferenceException;
+import com.klimjavadev.todolist.models.State;
+import com.klimjavadev.todolist.repositories.StateRepository;
+import com.klimjavadev.todolist.services.StateService;
+import jakarta.persistence.EntityNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+
+public class StateServiceImpl implements StateService {
+    private final StateRepository stateRepository;
+
+    public StateServiceImpl(StateRepository stateRepository) {
+        this.stateRepository = stateRepository;
+    }
+
+    @Override
+    public State create(State state) {
+        if (state != null) {
+            return stateRepository.save(state);
+        }
+        throw new NullEntityReferenceException("State cannot be 'null'");
+    }
+
+    @Override
+    public State readById(long id) {
+        return stateRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("State with id " + id + " not found"));
+    }
+
+    @Override
+    public State update(State state) {
+        if (state != null) {
+            readById(state.getId());
+            return stateRepository.save(state);
+        }
+        throw new NullEntityReferenceException("State cannot be 'null'");
+    }
+
+    @Override
+    public void delete(long id) {
+        State state = readById(id);
+        stateRepository.delete(state);
+    }
+
+    @Override
+    public List<State> getAll() {
+        return stateRepository.findByOrderByIdAsc();
+    }
+
+    @Override
+    public State getByName(String name) {
+        Optional<State> optional = Optional.ofNullable(stateRepository.findByName(name));
+        if (optional.isPresent()) {
+            return optional.get();
+        }
+        throw new EntityNotFoundException("State with name '" + name + "' not found");
+    }
+}
---
+++ src/main/java/com/klimjavadev/todolist/services/impl/ToDoServiceImpl.java
@@ -0,0 +1,82 @@
+package com.klimjavadev.todolist.services.impl;
+
+import com.klimjavadev.todolist.exceptions.NullEntityReferenceException;
+import com.klimjavadev.todolist.models.ToDo;
+import com.klimjavadev.todolist.models.User;
+import com.klimjavadev.todolist.repositories.ToDoRepository;
+import com.klimjavadev.todolist.services.ToDoService;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+
+import java.util.List;
+
+public class ToDoServiceImpl implements ToDoService {
+    private final ToDoRepository todoRepository;
+
+    public ToDoServiceImpl(ToDoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    @Override
+    public ToDo create(ToDo todo) {
+        if (todo != null) {
+            return todoRepository.save(todo);
+        }
+        throw new NullEntityReferenceException("ToDo cannot be 'null'");
+    }
+
+
+    @Override
+    public ToDo readById(long id) {
+        return todoRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("ToDo with id " + id + " not found"));
+    }
+
+    @Override
+    public ToDo update(ToDo todo) {
+        if (todo != null) {
+            readById(todo.getId());
+            return todoRepository.save(todo);
+        }
+        throw new NullEntityReferenceException("ToDo cannot be 'null'");
+    }
+
+    @Override
+    public void delete(long id) {
+        ToDo todo = readById(id);
+        todoRepository.delete(todo);
+    }
+
+    @Override
+    public List<ToDo> getAll() {
+        return todoRepository.findAll();
+    }
+
+    @Override
+    public List<ToDo> getByUserId(long userId) {
+        return todoRepository.getByUserId(userId);
+    }
+
+    @Override
+    public ToDo addCollaborator (User user, ToDo toDo){
+        List<User> collaborators = toDo.getCollaborators();
+        if (collaborators.contains(user)){
+            throw new EntityExistsException("Collaborator already exists.");
+        }
+        collaborators.add(user);
+        toDo.setCollaborators(collaborators);
+        return this.update(toDo);
+    }
+
+    @Override
+    public ToDo deleteCollaborator (User user, ToDo toDo){
+        List<User> collaborators = toDo.getCollaborators();
+        if(collaborators.contains(user)){
+            collaborators.remove(user);
+            toDo.setCollaborators(collaborators);
+            return update(toDo);
+        } else {
+            throw new EntityNotFoundException("User are not collaborator");
+        }
+    }
+}
