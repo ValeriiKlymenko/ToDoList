0b1b9b329af74bbf601efef55aa05b1eeb5e0793
KlimJavaDev <klimjavadev@gmail.com>
2023-09-05 15:21:29 +0300

    Підготовка до злиття з main

--- src/main/java/com/klimjavadev/todolist/controllers/UserController.java
+++ src/main/java/com/klimjavadev/todolist/controllers/UserController.java
@@ -1,25 +1,19 @@
 package com.klimjavadev.todolist.controllers;
 
 import com.klimjavadev.todolist.dto.UserResponse;
+import com.klimjavadev.todolist.models.User;
 import com.klimjavadev.todolist.services.ToDoService;
 import com.klimjavadev.todolist.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    UserService userService;
 
-    @GetMapping
-    List<UserResponse> getAll() {
-        return userService.getAll().stream()
-                .map(UserResponse::new)
-                .collect(Collectors.toList());
-    }
 }
