995f0c389b965b9c4172bc5f71ab2442056f1307
KlimJavaDev <klimjavadev@gmail.com>
2023-09-05 11:43:46 +0300

    Додано models : Priority, Role, State, Task, ToDo, User

---
+++ src/main/java/com/klimjavadev/todolist/models/ToDo.java
@@ -0,0 +1,40 @@
+package com.klimjavadev.todolist.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "todos")
+public class ToDo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "The 'title' cannot be empty")
+    @Column(name = "title", nullable = false, unique = true)
+    private String title;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @OneToMany(mappedBy = "todo", cascade = CascadeType.REMOVE)
+    private List<Task> tasks;
+
+    @ManyToMany
+    @JoinTable(name = "todo_collaborator",
+            joinColumns = @JoinColumn(name = "todo_id"),
+            inverseJoinColumns = @JoinColumn(name = "collaborator_id"))
+    private List<User> collaborators;
+}
---
+++ src/main/java/com/klimjavadev/todolist/models/User.java
@@ -0,0 +1,59 @@
+package com.klimjavadev.todolist.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Pattern;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Pattern(regexp = "[A-Z][a-z]+",
+            message = "Must start with a capital letter followed by one or more lowercase letters")
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Pattern(regexp = "[A-Z][a-z]+",
+            message = "Must start with a capital letter followed by one or more lowercase letters")
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Pattern(regexp = "[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}",
+            message = "Must be a valid e-mail address")
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Pattern(regexp = "[A-Za-z\\d]{6,}",
+            message = "Must be minimum 6 symbols long, using digits and latin letters")
+    @Pattern(regexp = ".*\\d.*",
+            message = "Must contain at least one digit")
+    @Pattern(regexp = ".*[A-Z].*",
+            message = "Must contain at least one uppercase letter")
+    @Pattern(regexp = ".*[a-z].*",
+            message = "Must contain at least one lowercase letter")
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.REMOVE)
+    private List<ToDo> myTodos;
+
+    @ManyToMany
+    @JoinTable(name = "todo_collaborator",
+            joinColumns = @JoinColumn(name = "collaborator_id"),
+            inverseJoinColumns = @JoinColumn(name = "todo_id"))
+    private List<ToDo> otherTodos;
+}
+
